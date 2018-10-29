adda637a123618f848fba0913994add9c6a3cbff
植田智加来 <yakyuzukiueta@gmail.com>
2018-10-29 19:13:37 +0900

    config application3

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Chain
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.assets.initialize_on_precompile = false #付け加えた
   end
-  # config.assets.initialize_on_precompile = false #付け加えた
+
 end
